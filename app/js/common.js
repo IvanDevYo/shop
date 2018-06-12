cd5341317b31cb62859346064cfe71c9e22dc5a9
Ivan <ivan.incweb@gmail.com>
2018-06-12 09:33:58 +0300

    12.06

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
-	// Custom JS
+	$(".catalog__menu-item").mouseover(function() {
+		var th = $(this).find(".catalog__menu_hover");
+		$(th).css('display', 'block');
+	});
+
+	$(".catalog__menu-item").mouseleave(function() {
+		$(".catalog__menu_hover").hide();
+	});
 
 });
