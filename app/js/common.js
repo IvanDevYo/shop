17c2159367b63dbefa92eba947f3c20f4f735038
Ivan <ivan.incweb@gmail.com>
2018-06-12 14:34:50 +0300

    home page complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,25 @@
 $(function() {
 
 	$(".catalog__menu-item").mouseover(function() {
-		var th = $(this).find(".catalog__menu_hover");
-		$(th).css('display', 'block');
+		var th = $(this).find(".catalog__menu_hover"),
+			l = $(th).children(".catalog__menu-item_hover").length;
+		if (l >= 4) {
+			$(th).css('width', '882px');
+		} else if(l > 2) {
+			$(th).css('width', '667px');
+		} else if(l > 1) {
+			$(th).css('width', '452px');
+		}
+		$(th).css('display', 'flex');
 	});
 
 	$(".catalog__menu-item").mouseleave(function() {
 		$(".catalog__menu_hover").hide();
+	});
+
+	$(".mobile__menu-link").click(function(e) {
+		e.preventDefault();
+		$(".top__nav-nav").toggleClass("opened");
 	});
 
 });
